5afdc25a2eb2ab563ab1837f939101160fd62b1f
tanghong <522307026@qq.com>
2023-02-20 09:49:00 +0800

    优化

--- THyoBlogsWeb/src/adminViews/user/list.vue
+++ THyoBlogsWeb/src/adminViews/user/list.vue
@@ -150,7 +150,7 @@
           </el-col>
           <el-col :span="12">
             <el-form-item label="状态：">
-              <el-select v-model="userForm.roleId">
+              <el-select v-model="userForm.state">
                 <el-option label="启用" :value="1"> </el-option>
                 <el-option label="停用" :value="0"> </el-option>
               </el-select>
@@ -204,7 +204,7 @@ export default {
         roleId: "",
         account: "",
         password: "",
-        state: "",
+        state: 1,
       };
     };
     let userForm = reactive(userFormData());
