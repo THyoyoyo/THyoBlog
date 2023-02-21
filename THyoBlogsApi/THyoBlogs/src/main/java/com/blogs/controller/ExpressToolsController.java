27a05d366580631b4adda37d93e828046e3c6862
tanghong <522307026@qq.com>
2023-02-21 17:53:46 +0800

    发送邮箱

--- THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/controller/ExpressToolsController.java
+++ THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/controller/ExpressToolsController.java
@@ -5,15 +5,21 @@ import com.alibaba.fastjson.JSON;
 import com.blogs.annotation.Token;
 import com.blogs.model.expressTools.Botany;
 import com.blogs.model.expressTools.Translate;
+import com.blogs.model.expressTools.UpMail;
 import com.blogs.util.MD5;
 import com.blogs.vo.common.R;
 import com.sun.xml.internal.ws.api.model.ExceptionType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +28,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/expressTools")
 @Api(tags = "扩展工具")
 public class ExpressToolsController {
+
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    @Value("${spring.mail.username}")
+    private String from;
 
     @ApiOperation(value = "百度翻译")
     @PostMapping("/getTranslate")
@@ -117,6 +133,23 @@ public class ExpressToolsController {
             return R.succeed(parse);
         } catch (Throwable e) {
             return R.failed(403, "发生错误,请稍后再试");
+        }
+    }
+
+    @ApiOperation(value = "发送邮件")
+    @PostMapping("/upMail")
+    public R upMail(@RequestBody UpMail dto) throws MessagingException {
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(dto.getToEmail());//目标邮箱
+            simpleMailMessage.setSubject(dto.getUbject());
+            simpleMailMessage.setText(dto.getText());//正文
+            simpleMailMessage.setFrom(from);//发送的邮箱
+            mailSender.send(simpleMailMessage);
+            return  R.succeed();
+        }catch (Exception e){
+            System.out.println(e);
+            return  R.failed(404,"邮件发送失败");
         }
     }
 }
--- THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/model/expressTools/UpMail.java
+++ THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/model/expressTools/UpMail.java
@@ -1,2 +1,15 @@
-package com.blogs.model.expressTools;public class UpMail {
+package com.blogs.model.expressTools;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+@Data
+public class UpMail {
+    @ApiModelProperty(value = "收件人")
+    private String toEmail;
+    @ApiModelProperty(value = "主题")
+    private String ubject;
+    @ApiModelProperty(value = "内容")
+    private String text;
 }
