3ca2d84bb015283c04f7bcde8c81f12a4f9bc1ae
tanghong <522307026@qq.com>
2023-02-23 14:40:50 +0800

    统一图像识别

--- THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/model/expressTools/ImageScan.java
+++ THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/model/expressTools/ImageScan.java
@@ -6,14 +6,15 @@ import io.swagger.models.auth.In;
 import lombok.Data;
 
 @Data
-public class Botany {
+public class ImageScan {
+    @ApiModelProperty(value = "1:植物识别,2：通用物体和场景识别")
+    private String type;
+
     @ApiModelProperty(value = "图片网络链接(二选一)")
     private String imgUrl;
 
     @ApiModelProperty(value = "图片base64(二选一)")
     private String imgBase64;
-
-
 
     @ApiModelProperty(value = "是否返回百科")
     private String baikeNum;
