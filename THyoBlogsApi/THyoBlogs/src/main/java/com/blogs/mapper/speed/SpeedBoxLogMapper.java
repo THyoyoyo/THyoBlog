581aefe03f0cd01b08b7729182e1de8b7ed41af0
tanghong <522307026@qq.com>
2023-05-09 17:40:48 +0800

    飞车工具箱新增-领取app福利、自动开箱记录

---
+++ THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/mapper/speed/SpeedBoxLogMapper.java
@@ -0,0 +1,11 @@
+package com.blogs.mapper.speed;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.blogs.model.speed.SpeedBoxLog;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SpeedBoxLogMapper extends BaseMapper<SpeedBoxLog> {
+
+
+}
---
+++ THyoBlogsApi/THyoBlogs/src/main/java/com/blogs/model/speed/SpeedBoxLog.java
@@ -0,0 +1,56 @@
+package com.blogs.model.speed;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.lang.Nullable;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 数据库映射类
+ * 对应表名: speed_box_log
+ * @author 映射类自动生成器-LXY
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@TableName("speed_box_log")
+public class SpeedBoxLog implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/** 自增id */
+	@TableId(type = IdType.AUTO)
+	@ApiModelProperty(value = "自增id")
+	private Integer id;
+
+	/** 归属记录id */
+	@ApiModelProperty(value = "归属记录id")
+	private Integer speedId;
+
+	/** 开箱数据 */
+	@ApiModelProperty(value = "开箱数据")
+	private String json;
+
+	/** 创建时间 */
+	@ApiModelProperty(value = "创建时间")
+	private Date created;
+
+
+	/** 列表数据 */
+	@ApiModelProperty(value = "列表数据")
+	@TableField(exist = false)
+	private List<Map<String,Object>> dataList;
+}
